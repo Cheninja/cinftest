6c5e3bb2df33c9219b7e6ab60443bc36c339abe8
Cheninja <103942096+Cheninja@users.noreply.github.com>
2022-04-22 13:25:42 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,23 +1,30 @@
-import pytest
+import pytest 
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
+	
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
-
-
+	# задаем опцию для запуска "браузер": если не указать, то запускается Chrome
+	parser.addoption('--browser_name', action='store', default='chrome', help="Choose browser: firefox or chrome")
+	# задаем опцию для запуска "язык": ru, fr и тд.
+	parser.addoption('--language', action='store', default=None, help="Say language name to select")
+	
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
+
+	# считываем язык и браузер (если есть)
+	choosen_language = request.config.getoption("language")
+	browser_name = request.config.getoption("browser_name")
+	if browser_name == "chrome":
+		# инициализируем браузер chrome с нужными опциями
+		opts = Options()
+		opts.add_experimental_option('prefs', {'intl.accept_languages': choosen_language})
+		opts.add_experimental_option('w3c', False)
+		browser = webdriver.Chrome(options=opts)
+	elif browser_name == "firefox":
+	    # инициализируем браузер firefox с нужными опциями
+		fp = webdriver.FirefoxProfile()
+		fp.set_preference("intl.accept_languages", choosen_language)
+		browser = webdriver.Firefox(firefox_profile=fp)
+	yield browser
+	# закрытие браузера после работы
+	browser.quit()
